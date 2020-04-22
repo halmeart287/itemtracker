f6b93e5d7cbaee08e1bef9a5ef198f30c6949cbf
Arttu Halmetoja <bgh287@myy.haaga-helia.fi>
2020-04-22 14:21:31 +0300

    Added editing and deleting options.

--- src/main/java/hh0/itemtracker/web/ItemController.java
+++ src/main/java/hh0/itemtracker/web/ItemController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,11 +20,10 @@ public class ItemController {
 
 	@Autowired
 	private ItemRepository itemRepository;
-	
+
 	@Autowired
 	private TrackerRepository trackerRepository;
-	
-	
+
 	// Lista 'Item' -luokan olioista.
 	@RequestMapping(value = "/items", method = RequestMethod.GET)
 	public String getItems(Model model) {
@@ -31,7 +31,7 @@ public class ItemController {
 		model.addAttribute("items", items);
 		return "items";
 	}
-	
+
 	// 'Item' -olion lisäyslomake.
 	@RequestMapping(value = "/additem", method = RequestMethod.GET)
 	public String getItemForm(Model model) {
@@ -39,12 +39,37 @@ public class ItemController {
 		model.addAttribute("trackvalues", trackerRepository.findAll());
 		return "additem";
 	}
-	
+
 	// 'Item' -olion tallennuspyyntö.
 	@RequestMapping(value = "/saveitem", method = RequestMethod.POST)
 	public String saveItem(Item item) {
 		itemRepository.save(item);
 		return "redirect:items";
 	}
-	
+
+	// 'Item' -olion poisto.
+	@RequestMapping(value = "/deleteitem/{itemId}", method = RequestMethod.GET)
+	public String deleteItem(@PathVariable("itemId") Long itemId) {
+		itemRepository.deleteById(itemId);
+		return "redirect:../items";
+	}
+
+	// 'Item' -olion muokkaus.
+	@RequestMapping(value = "/edititem/{itemId}", method = RequestMethod.GET)
+	public String editItem(@PathVariable("itemId") Long itemId, Model model) {
+		model.addAttribute("item", itemRepository.findById(itemId));
+		model.addAttribute("trackvalues", trackerRepository.findAll());
+		return "edititem";
+	}
+
+	// ToDo: 'Item' -olion poistaminen tracking-listalta napin painalluksella ilman
+	// erillistä muokkausikkunaa.
+
+	/*
+	 * @RequestMapping(value = "/removetrack", method = RequestMethod.POST) public
+	 * String removeTrack(@PathVariable("itemId") Long itemId, Model model) {
+	 * model.addAttribute("item", itemRepository.findById(itemId)); return "items";
+	 * }
+	 */
+
 }
